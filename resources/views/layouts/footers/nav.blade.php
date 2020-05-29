8cee09269f58f3b0b2da5a426669f1c8ed1919f4
aisyahhanifiah <aisyahhanifiah@live.com>
2020-05-29 13:25:52 +0800

    commit

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -14,13 +14,13 @@
                 <a href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
             </li> --}}
             <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About ClubHub</a>
+                <a href="https://github.com/aisyahhanifiah/unikl-clubhub" class="nav-link" target="_blank">About ClubHub</a>
             </li>
            {{--  <li class="nav-item">
                 <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
             </li> --}}
             <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                <a href="https://github.com/aisyahhanifiah/unikl-clubhub/blob/f386c79b9499384ff9e30d8b4c3a5d81e02df9d3/LICENSE" class="nav-link" target="_blank">GNU License</a>
             </li>
         </ul>
     </div>
